5eed1a78d26ac094e741ff53ac51e2c6ea8c2b14
priya-shakya <asthashakya751@gmail.com>
2025-06-20 13:20:35 +0530

    Create client>src>App.vue

---
+++ client>src>App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ChatComponent />
+  </div>
+</template>
+
+<script>
+import ChatComponent from './components/ChatComponent.vue'
+
+export default {
+  components: {
+    ChatComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
